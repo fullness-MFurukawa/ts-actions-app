58b1d9b96dc3d269268de6b99e1a6b37da825417
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-04-20 15:07:32 +0900

    電卓コンポーネントとコンポーネントテストを追加する

--- src/hooks/useCalculator.ts
+++ src/hooks/useCalculator.ts
@@ -44,7 +44,8 @@ export const useCalculator = () => {
 
       // 計算に成功した場合は、結果を状態にセットして画面に反映させる
       setResult(res);
-    } catch (err: unknown) { // any ではなく unknown を使用する（または省略する）
+    } catch (err: unknown) {
+      // any ではなく unknown を使用する（または省略する）
       // 0除算(ゼロで割る)などの例外が発生した場合は、エラーメッセージを画面に表示する
       // err が Error オブジェクトかどうかを判定（Type Guard）
       if (err instanceof Error) {
