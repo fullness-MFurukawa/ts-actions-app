24fd99d9cfbcfbe812ea0a3b65a44e49b102b046
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-04-20 18:04:29 +0900

    PlaywrightのE2Eテストを追加する

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
@@ -8,6 +8,8 @@ export default defineConfig({
     environment: 'jsdom', // Node.js上でブラウザのDOMをシミュレートする
     setupFiles: ['./vitest.setup.ts'], // テスト実行前に読み込む設定ファイル
     globals: true,
+    // デフォルトの除外リストに 'tests/**' (Playwright用フォルダ) を追加します
+    exclude: [...configDefaults.exclude, 'tests/**'],  
   },
   resolve: {
     alias: {
