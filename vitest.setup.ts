1e81c80357471b8ebf59fa16169acb57bafd80cc
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-04-20 15:16:36 +0900

    電卓コンポーネントとコンポーネントテストを追加する

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,2 +1,8 @@
 // テスト時にtoBeInTheDocument()などの便利なカスタムマッチャーを使えるようにする
 import '@testing-library/jest-dom'
+// JSDOM環境に不足している PointerEvent 関連のAPIを擬似的にモックする
+if (typeof window !== 'undefined') {
+  window.HTMLElement.prototype.hasPointerCapture = () => false;
+  window.HTMLElement.prototype.setPointerCapture = () => {};
+  window.HTMLElement.prototype.releasePointerCapture = () => {};
+}
