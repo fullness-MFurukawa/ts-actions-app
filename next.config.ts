3008cf54f4a9970d99580d7e64816a9abf948484
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-04-29 19:19:27 +0900

    Webサーバーの連携とアプリケーションの起動

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 以下の1行を追記！
+  output: 'standalone',
+  // (もし他に設定が書いてあれば、それはそのまま残しておいてください)
 };
 
 export default nextConfig;
