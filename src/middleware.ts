b3174de9473c05124639bb3702ca08a7ac546e36
Masatoshi Furukawa <furukawa@fullness.co.jp>
2026-05-01 16:49:01 +0900

    Webサーバーの連携とアプリケーションの起動

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export { default } from "next-auth/middleware";
 export const config = {
-  // 保護したい（ログイン必須にしたい）ページのパスを指定します
-  // 例: トップページ(/) と 計算画面(/Calc) と 検索画面(/search) を保護
+  // 保護したい(ログイン必須にしたい)ページのパスを指定する
+  // 例: トップページ(/)と計算画面(/Calc)と検索画面(/search)を保護
   matcher: ["/", "/Calc", "/search"],
 };
